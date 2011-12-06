fdad6c42cc9daf829d35f4864a8503d5b8c292a1
J Parker <kornyone@gmail.com>
2011-12-05 23:38:55 -0600

    Cleaning up differences between Shooter and Doubleshot for kgsl changes brought in 8ce043969b100dedb548c280562932ac3068491f

--- drivers/video/msm_8x60/gpu/kgsl_adreno220/kgsl_yamato.c
+++ drivers/video/msm_8x60/gpu/kgsl_adreno220/kgsl_yamato.c
@@ -537,7 +537,6 @@ static int kgsl_yamato_start(struct kgsl_device *device, unsigned int init_ram)
 	int status = -EINVAL;
 	struct kgsl_yamato_device *yamato_device = KGSL_YAMATO_DEVICE(device);
 	int init_reftimestamp = 0x7fffffff;
-	unsigned int override1, override2, i;
 
 	device->state = KGSL_STATE_INIT;
 	device->requested_state = KGSL_STATE_NONE;
